957f408d3a9f2ee5566b752bc75c596bd50723e1
Kein Niemand <18308201+KeinNiemand@users.noreply.github.com>
2022-10-23 20:18:37 +0200

    fixed landfill painting names in settings

--- settings.lua
+++ settings.lua
@@ -5,11 +5,11 @@ local isLandfillPaintingLoaded = mods["LandfillPainting"]
 local lanfillTypes = { "landfill" }
 
 if (isLandfillPaintingLoaded) then
-    table.insert(lanfillTypes, "landfill-dry-dirt")
-    table.insert(lanfillTypes, "landfill-dirt-4")
-    table.insert(lanfillTypes, "landfill-grass-1")
-    table.insert(lanfillTypes, "landfill-red-desert-1")
-    table.insert(lanfillTypes, "landfill-sand-3")
+    table.insert(lanfillTypes, "dry-dirt")
+    table.insert(lanfillTypes, "dirt-4")
+    table.insert(lanfillTypes, "grass-1")
+    table.insert(lanfillTypes, "red-desert-1")
+    table.insert(lanfillTypes, "sand-3")
 end
 
 
